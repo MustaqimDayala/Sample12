e06ce859116a6cf900c54778a011669a3a42908a
MustaqimDayala <56254795+MustaqimDayala@users.noreply.github.com>
2019-10-09 17:42:43 +0530

    Add files via upload

---
+++ Sample_Scala_Code.scala
@@ -0,0 +1,19 @@
+/**Scala program to find a number is positive, negative or positive.*/
+
+    object excheckNumber {
+       def main(args: Array[String]) {
+        
+            /**declare a variable*/
+            var Number= (-100);
+       
+           if(Number==0){
+               println("Number is zero")
+           }
+           else if(Number>0){
+               println("Number is positive")
+           }
+           else{
+               println("Number is negative")
+           }
+       }
+    }
